855906501acad47d7feab3568d7e44771729a270
Camden McKay <cam.mckay9@gmail.com>
2023-11-06 23:38:56 -0600

    fixed semicolon

--- src/index.php
+++ src/index.php
@@ -4,9 +4,7 @@ $dbDatabase = getenv('AZURE_MYSQL_DBNAME') ?: 'project';
 $dbUsername = getenv('AZURE_MYSQL_USERNAME') ?: 'root';
 $dbPassword = getenv('AZURE_MYSQL_PASSWORD') ?: 'root';
 $dbPort = getenv('AZURE_MYSQL_PORT') ?: '3306';
-$dbAttrs = [
-    PDO::ATTR_PERSISTENT => true;
-];
+$dbAttrs = array(PDO::ATTR_PERSISTENT => true);
 if (getenv('MYSQL_ATTR_SSL_CA')) {
     $dbAttrs[PDO::MYSQL_ATTR_SSL_CA] = getenv('MYSQL_ATTR_SSL_CA');
     $dbAttrs[PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT] = true;
