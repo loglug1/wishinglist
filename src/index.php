ffde3598dbda6f7785d3ab23fdb74283c1b646b8
Camden McKay <cam.mckay9@gmail.com>
2023-11-06 23:32:10 -0600

    added certificate for mysql server

--- src/index.php
+++ src/index.php
@@ -4,12 +4,18 @@ $dbDatabase = getenv('AZURE_MYSQL_DBNAME') ?: 'project';
 $dbUsername = getenv('AZURE_MYSQL_USERNAME') ?: 'root';
 $dbPassword = getenv('AZURE_MYSQL_PASSWORD') ?: 'root';
 $dbPort = getenv('AZURE_MYSQL_PORT') ?: '3306';
+$dbAttrs = [
+    PDO::ATTR_PERSISTENT => true;
+];
+if (getenv('MYSQL_ATTR_SSL_CA')) {
+    $dbAttrs[PDO::MYSQL_ATTR_SSL_CA] = getenv('MYSQL_ATTR_SSL_CA');
+    $dbAttrs[PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT] = true;
+}
 
 try {
-    $pdo = new PDO("mysql:host={$dbHost};dbname={$dbName};port={$dbPort}", $dbUsername, $dbPassword, array(PDO::ATTR_PERSISTENT => true));
+    $pdo = new PDO("mysql:host={$dbHost};dbname={$dbName};port={$dbPort}", $dbUsername, $dbPassword, $dbAttrs);
 } catch (PDOException $e) {
-    var_dump($e);
-    die();
+    die($e->fetchMessage());
 }
 echo "Hello World! 2";
 ?>
